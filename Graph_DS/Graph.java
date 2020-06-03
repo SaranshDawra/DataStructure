6a213116f09641a1f3ea52d91baa07ca0a082820
SaranshDawra <saransh2000dawra@gmail.com>
2020-06-03 16:24:14 +0530

    BFS Traversal added

--- Graph_DS/Graph.java
+++ Graph_DS/Graph.java
@@ -1,6 +1,8 @@
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Map.Entry;
+import java.util.Queue;
 
 class Graph <T>{
 	
@@ -25,6 +27,29 @@ class Graph <T>{
 			map.get(dest).add(src);
 		}
 			
+	}
+
+	public void bft() {
+		T src = (T) map.keySet().toArray()[0];
+		bft(src);
+	}
+
+	public void bft(T src) {
+		Queue<T> q = new LinkedList<>();
+		HashSet<T> set = new HashSet<>();
+		q.add(src);
+		set.add(src);
+		while (!q.isEmpty()) {
+			T temp = q.poll();
+			System.out.print(temp + "  ");
+			LinkedList<T> list = map.get(temp);
+			for (int i = 0; i < list.size(); i++) {
+				if (!set.contains(list.get(i))) {
+					set.add(list.get(i));
+					q.add(list.get(i));
+				} 
+			}
+		}
 	}
 	
 	public void print() {
