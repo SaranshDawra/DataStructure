f5de3aa354b370d6fac94e6b48f8f1e7151ae1bd
SaranshDawra <saransh2000dawra@gmail.com>
2020-06-07 20:21:40 +0530

    Updated

--- Graph_DS/graphClient.java
+++ Graph_DS/graphClient.java
@@ -12,20 +12,28 @@ public class graphClient {
 //		graph.add(5, 3, true);
 //		graph.add(0, 5, true);
 		
-		graph.add(0, 1, false); 
-		graph.add(0, 2, false); 
-		graph.add(1, 2, false); 
-		graph.add(2, 0, false); 
-		graph.add(2, 3, false); 
-		graph.add(3, 3, false);
+		graph.add(0, 1, true);
+		graph.add(1, 2, true);
+		graph.add(0, 4, true);
+		graph.add(2, 4, true);
+		graph.add(3, 2, true);
+		graph.add(3, 5, true);
+		graph.add(3, 4, true);
 		
-//		graph.print();
-//		graph.bft(4);
-//		System.out.println();
-		graph.bft();
-		System.out.println();
+//		graph.add(0, 1, false); 
+//		graph.add(0, 2, false); 
+//		graph.add(1, 2, false); 
+//		graph.add(2, 0, false); 
+//		graph.add(2, 3, false); 
+//		graph.add(3, 3, false);
+		
+		graph.print();
+		System.out.println("DFS");
 		graph.dft(2);
-
+		System.out.println();
+		System.out.println("BFS");
+		graph.bft(0, 5);
+		
 	}
 
 }
