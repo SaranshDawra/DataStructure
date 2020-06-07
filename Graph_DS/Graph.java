17aef98a0a0931a4753a6ba7c99a0a107b8948f0
SaranshDawra <saransh2000dawra@gmail.com>
2020-06-07 20:21:09 +0530

    Updated

--- Graph_DS/Graph.java
+++ Graph_DS/Graph.java
@@ -4,87 +4,107 @@ import java.util.LinkedList;
 import java.util.Map.Entry;
 import java.util.Queue;
 
-class Graph <T>{
-	
-	private HashMap<T, LinkedList<T>> map;
-	
+class Graph {
+
+	private HashMap<Integer, LinkedList<Integer>> map;
+
 	Graph() {
 		map = new HashMap<>();
 	}
-	
-	public void add(T src, T dest, boolean direction) {
-		
-		if(!map.containsKey(src)) {
-			map.put(src, new LinkedList<T>());
+
+	public void add(int src, int dest, boolean direction) {
+
+		if (!map.containsKey(src)) {
+			map.put(src, new LinkedList<Integer>());
 		}
-		
-		if(!map.containsKey(dest)) {
-			map.put(dest, new LinkedList<T>());
+
+		if (!map.containsKey(dest)) {
+			map.put(dest, new LinkedList<Integer>());
 		}
-		
+
 		map.get(src).add(dest);
-		if(direction == true) {
+		if (direction == true) {
 			map.get(dest).add(src);
 		}
-			
+
 	}
 
-	public void bft() {
-		T src = (T) map.keySet().toArray()[0];
-		bft(src);
-	}
+//	public void bft() {
+//		T src = (T) map.keySet().toArray()[0];
+//		bft(src);
+//	}
 
-	public void bft(T src) {
-		Queue<T> q = new LinkedList<>();
-		HashSet<T> set = new HashSet<>();
+	public void bft(int src, int dest) {
+		Queue<Integer> q = new LinkedList<>();
+		HashSet<Integer> set = new HashSet<>();
+		HashMap<Integer, Integer> dist = new HashMap<>();
+		HashMap<Integer, Integer> parent = new HashMap<>();
+		System.out.println("BFS traversal starting from " + src);
 		q.add(src);
 		set.add(src);
+		dist.put(src, 0);
+		parent.put(src, -1);
 		while (!q.isEmpty()) {
-			T temp = q.poll();
+			int temp = q.poll();
 			System.out.print(temp + "  ");
-			LinkedList<T> list = map.get(temp);
+			LinkedList<Integer> list = map.get(temp);
 			for (int i = 0; i < list.size(); i++) {
 				if (!set.contains(list.get(i))) {
 					set.add(list.get(i));
 					q.add(list.get(i));
-				} 
+					dist.put(list.get(i), dist.get(temp) + 1);
+					parent.put(list.get(i), temp);
+				}
 			}
+		}
+		System.out.println();
+		System.out.println("Distance of each vertex from " + src);
+		System.out.println(dist);
+		System.out.println("Path from destination: " + dest +" to source: "+ src);
+		System.out.println(parent);
+		int p = parent.get(dest);
+		int t = dest;
+		System.out.print(t+" ");
+		while(p != -1) {
+			t = p;
+			p = parent.get(p);
+			System.out.print(t+" ");
 		}
 	}
 
 	public void dft() {
-		T src = (T) map.keySet().toArray()[0];
-		HashSet<T> set = new HashSet<>();
+		int src = (Integer) map.keySet().toArray()[0];
+		HashSet<Integer> set = new HashSet<>();
 		dft(src, set);
 	}
 
-	public void dft(T src) {
-		HashSet<T> set = new HashSet<>();
+	public void dft(int src) {
+		HashSet<Integer> set = new HashSet<>();
 		dft(src, set);
 	}
 
-	private void dft(T src, HashSet<T> set) {
+	private void dft(int src, HashSet<Integer> set) {
 		set.add(src);
 		System.out.print(src + "  ");
 
-		LinkedList<T> lst = map.get(src);
+		LinkedList<Integer> lst = map.get(src);
 		for (int i = 0; i < lst.size(); i++) {
 			if (!set.contains(lst.get(i))) {
 				dft(lst.get(i), set);
 			}
 		}
 	}
-	
+
 	public void print() {
 //		for(T key: map.keySet()) {
 //			System.out.print(key + "-->");
 //			System.out.println(map.get(key));
 //		}
-		
-		for(Entry<T, LinkedList<T>> mapElement : map.entrySet()) {
-			T key = mapElement.getKey();
+
+		for (Entry<Integer, LinkedList<Integer>> mapElement : map.entrySet()) {
+			int key = mapElement.getKey();
 			System.out.print(key + "-->");
-			LinkedList<T> value = mapElement.getValue();
+			LinkedList<Integer> value = mapElement.getValue();
 			System.out.println(value);
 		}
 	}
