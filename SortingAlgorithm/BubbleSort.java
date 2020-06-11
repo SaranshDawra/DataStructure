cd7c25e8f5f4d815736b9f29936cdb69c61e39ef
SaranshDawra <saransh2000dawra@gmail.com>
2020-06-12 02:05:02 +0530

    Bubble Sort Resursive

--- SortingAlgorithm/BubbleSort.java
+++ SortingAlgorithm/BubbleSort.java
@@ -12,7 +12,6 @@ public class BubbleSort {
 		bubbleSort(arr);
 		for (int i = 0; i < n; i++) {
 			System.out.print(arr[i] + " ");
-			;
 		}
 	}
 
@@ -31,5 +30,21 @@ public class BubbleSort {
 				break;
 			}
 		}
+	}
+
+	public static void bubbleSortRecursive(int arr[], int n) {
+		if (n == 1) {
+			return;
+		}
+
+		for (int i = 0; i < n - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				int temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+			}
+		}
+
+		bubbleSortRecursive(arr, n - 1);
 	}
 }
